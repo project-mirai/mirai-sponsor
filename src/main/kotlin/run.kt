46f785b2608d8127cf5959956d6bee26937784b8
RainChan <mzdluo123@foxmail.com>
2022-06-23 05:30:57 -0700

    action一键添加记录 (#6)
    * update generator
    
    * fix
    
    * Update update.yml
    
    * Update update.yml
    
    * add gradle-wrapper.jar
    
    * Update log
    
    * fix commit build folder
    
    * fix commit build folder
    
    * Update log
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/main/kotlin/run.kt
+++ src/main/kotlin/run.kt
@@ -3,77 +3,109 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 //demo
-fun main(){
-    val transactions = loadTransactions()
+//fun main(){
+//    val transactions = loadTransactions()
+//
+//    transactions.add(buildTransaction {
+//        now()
+//        cost(64800)//-¥648.00
+//        operator = Operator("Him188","https://github.com/him188")
+//        remark = "打游戏"
+//    })
+//
+//    transactions.save()
+//    transactions.computeCashFlow().saveTo(File(FILE_BASE + "test.md"))
+//}
 
-    transactions.add(buildTransaction {
-        now()
-        cost(64800)//-¥648.00
-        operator = Operator("Him188","https://github.com/him188")
-        remark = "打游戏"
-    })
 
-    transactions.save()
-    transactions.computeCashFlow().saveTo(File(FILE_BASE + "test.md"))
+fun main() {
+    try {
+        add_trans()
+    }catch (e:IllegalArgumentException){
+        println("未设置环境变量，跳过添加新记录")
+    }
+    build()
 }
 
+fun add_trans() {
+    val env_map = System.getenv()
+    val cost = env_map["MIRAI_COST"]
+    val op_name = env_map["MIRAI_OP_NAME"]
+    val op_link = env_map["MIRAI_OP_LINK"]
+    val env_remark = env_map["REMARK"]
+    if (cost == null || op_name == null || env_remark == null) {
+        throw IllegalArgumentException("参数无效")
+    }
+    val transactions = loadTransactions()
+    transactions.add(buildTransaction {
+        now()
+        cost(cost.toLong())
+        operator = Operator(op_name, op_link)
+        remark = env_remark
+    })
+    transactions.save()
+}
+fun build(){
+    val transactions = loadTransactions()
+    transactions.computeCashFlow().save()
+}
 
-class TransactionBuilder{
+class TransactionBuilder {
 
-    var timeStamp:Long? = null
+    var timeStamp: Long? = null
 
-    fun now(){
+    fun now() {
         timeStamp = System.currentTimeMillis()
     }
 
-    fun yesterday(){
-        timeStamp = System.currentTimeMillis() - 24*60*60*1000
+    fun yesterday() {
+        timeStamp = System.currentTimeMillis() - 24 * 60 * 60 * 1000
     }
 
-    fun dayOf(year: Int, month:Int, day:Int){
+    fun dayOf(year: Int, month: Int, day: Int) {
         timeStamp = simpleDateFormat().parse("${year}/${month}/${day}").time
     }
 
-    var amount:Long? = null
+    var amount: Long? = null
 
-    fun income(amount:Long){
+    fun income(amount: Long) {
         this.amount = amount
     }
 
-    fun sponsor(amount:Long){
+    fun sponsor(amount: Long) {
         this.amount = amount
         this.remark = "赞助"
     }
 
-    fun outcome(amount:Long){
+    fun outcome(amount: Long) {
         this.amount = -amount
     }
 
-    fun cost(amount:Long){
+    fun cost(amount: Long) {
         this.amount = -amount
     }
 
     var operator: Operator? = null
 
-    var remark:String? = null
+    var remark: String? = null
 
-    var proof:Proof? = null
+    var proof: Proof? = null
 
-    data class Proof(val caption:String, val link:String)
+    data class Proof(val caption: String, val link: String)
 
 
-    fun build():Transaction{
+    fun build(): Transaction {
         return Transaction(
-            timestamp = timeStamp?: error("Timestamp can't be null"),
-            operator = operator?: error("Operator can't be null"),
-            amount = amount?: error("Amount can't be null"),
-            attributes = mutableMapOf<TransactionAttribute,String>().apply{
-                if(remark != null){
-                    put(TransactionAttribute.REMARK,remark!!)
+            timestamp = timeStamp ?: error("Timestamp can't be null"),
+            operator = operator ?: error("Operator can't be null"),
+            amount = amount ?: error("Amount can't be null"),
+            attributes = mutableMapOf<TransactionAttribute, String>().apply {
+                if (remark != null) {
+                    put(TransactionAttribute.REMARK, remark!!)
                 }
-                if(proof != null){
-                    put(TransactionAttribute.PROOF_LINK,proof!!.link)
-                    put(TransactionAttribute.PROOF_CAPTION,proof!!.caption)
+                if (proof != null) {
+                    put(TransactionAttribute.PROOF_LINK, proof!!.link)
+                    put(TransactionAttribute.PROOF_CAPTION, proof!!.caption)
                 }
             }
         )
@@ -81,4 +113,4 @@ class TransactionBuilder{
 }
 
 
-fun buildTransaction(block: TransactionBuilder.() -> Unit):Transaction = TransactionBuilder().apply(block).build()
+fun buildTransaction(block: TransactionBuilder.() -> Unit): Transaction = TransactionBuilder().apply(block).build()
